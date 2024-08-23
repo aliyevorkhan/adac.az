80e3da60d57d2dded67d09b99a1f1d54aa922f25
Orkhan Aliyev <006807@ibar.int>
2024-08-23 11:09:06 +0400

    removed cache

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("mgmt/", admin.site.urls),
     path("", include("core.urls")),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
