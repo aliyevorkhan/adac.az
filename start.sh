c4710bbaf77a75f7d95599848c20e45b34b22306
Orkhan Aliyev <006807@ibar.int>
2024-08-23 10:55:27 +0400

    added starter

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull
+. venv/bin/activate
+pip install -r requirements.txt
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+
+python3 manage.py runserver 0.0.0.0:8889
